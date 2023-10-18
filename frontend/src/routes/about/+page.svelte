f9294e1bee60621fe9e1b38bd8f63b0411bc5307
avicity7 <karlorjalo@gmail.com>
2023-10-18 22:18:54 +0800

    fix: About layout

--- frontend/src/routes/about/+page.svelte
+++ frontend/src/routes/about/+page.svelte
@@ -1,15 +1,17 @@
 <div class="font-albert max-w-xl">
   <h1 class="w-screen pt-12 pl-6 md:pl-24 font-bold text-3xl mb-24">About</h1>
-  <div class="ml-6 md:ml-24 flex flex-col items-left">
-    <h1 class="text-3xl md:text-4xl font-black text-neutral-800 mb-8">Let us remember for you.</h1>
-    <div class="text-lg md:text-xl mb-8">
-      We've all had that friend who claims they've paid us.
-      That friend who pays lesser than they owe you. 
+  <div class="ml-6 mr-6 md:mr-0 md:ml-24 flex flex-col items-left">
+    <h1 class="text-2xl md:text-4xl font-black text-neutral-800 mb-8">Let us remember for you.</h1>
+    <div class="text-md md:text-xl mb-8">
+      You've had that friend that swears he doesn't owe you any money.
+      <br>
+      <br> 
+      That friend that paid back lesser than he owes.
       <br>
       <br>
-      Let us remember those pesky details, and focus on getting that bag.
+      <p class="font-medium">Let us remember those pesky details, and focus on getting that bag.</p>
     </div>
-    <div class="w-[90%] md:w-full">
+    <div class="w-full">
       <a href={"/"} class="flex grow flex-row justify-center rounded p-1 font-semibold text-neutral-100 bg-purple-500 hover:bg-purple-600">Get Started</a>
     </div>
   </div>
