de0e35a2aee42d2d2d58f4da8bbe7a8b984f9ea9
avicity7 <karlorjalo@gmail.com>
2023-10-18 21:49:32 +0800

    feat: About page and home styling

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -2,8 +2,8 @@
   import "../app.css"
 </script>
 
-<nav class="p-6 text-xl bg-neutral-800">
-	<a href="/" class="mr-12 text-lg font-black text-neutral-300">whoowesme.</a>
+<nav class="p-6 text-lg bg-neutral-800 font-albert">
+	<a href="/" class="mr-6 font-black text-neutral-100">whoowesme.</a>
 	<a href="/" class="mr-6 font-medium text-neutral-100 hover:text-purple-300">home</a>
 	<a href="/about" class="mr-6 font-medium text-neutral-100 hover:text-purple-300">about</a>
 </nav>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -2,12 +2,17 @@
   let pageData = [{title:null, lender_user_id:null, borrower_user_id:null, amount:null, outstanding:null}]
   let name = '',
     amount = 0
+  let fetching = false
+  let sending = false
   const getData = async () => {
+    fetching = true
     let data = await fetch("https://whoowesme-server.onrender.com/records")
     let result = await data.json()
-    pageData = result
+    pageData = result.results
+    fetching = !fetching
   }
   const sendData = async() => {
+    sending = true
     const response = await fetch('https://whoowesme-server.onrender.com/records', {
 			method: 'POST',
 			body: JSON.stringify({title:name, lender_user_id: 0, borrower_user_id:0, amount, outstanding:true}),
@@ -15,33 +20,37 @@
 				'content-type': 'application/json'
 			}
 		});
+    getData()
+    sending = !sending
   }
 </script>
 
-<div class="font-albert m-16 flex flex-row justify-center text-neutral-800">
+<div class="font-albert text-neutral-800">
   <div class="flex flex-col items-center">
-    <h1 class="w-screen pl-24 font-bold text-3xl mb-24">Home</h1>
+    <h1 class="w-screen pt-12 pl-24 font-bold text-3xl mb-24">Home</h1>
     <form
       class="flex flex-col items-center"
       on:submit|preventDefault={sendData}
     >
-      <input 
-        type="text"
-        name="name"
-        autocomplete="off"
-        placeholder="Name"
-        class="ml-2 mb-6 outline-none select-none"
-        bind:value={name}
-      />
-
       <label
         class="font-medium"
+      >Name:
+        <input 
+          type="text"
+          name="name"
+          autocomplete="off"
+          class="p-1 ml-2 border-2 border-neutral-200 rounded focus:border-purple-400 outline-none select-none"
+          bind:value={name}
+        />
+
+      <label
+        class="font-medium ml-10 mr-10"
       >Amount:
         <input 
           type="number"
           name="amount"
           autocomplete="off"
-          class="ml-2 border-2 border-neutral-200 rounded focus:border-purple-400 outline-none select-none"
+          class="p-1 ml-2 border-2 border-neutral-200 rounded focus:border-purple-400 outline-none select-none"
           bind:value={amount}
         />
       </label>
@@ -49,7 +58,11 @@
         class="bg-neutral-800 text-neutral-100 font-semibold rounded hover:bg-purple-900 mt-8 p-2"
         type="submit"
       >
-        Submit
+        {#if sending}
+          Saving...
+        {:else}
+          Save
+        {/if}
       </button>
     </form>
 
@@ -57,13 +70,18 @@
       class="bg-neutral-800 text-neutral-100 font-semibold rounded hover:bg-purple-900 mt-8 p-2"
       on:click={getData}
     >
-      Click me!
+      {#if fetching}
+        Loading...
+      {:else}
+        Get records
+      {/if}
     </button>
 
     {#if pageData[0].title != null}
-      {#each pageData as item, i}
+      {#each pageData as item}
           <div class="shadow p-5 m-5 rounded">
-            {i}: {item.title}
+            <p class="text-lg font-semibold">{item.title}</p>
+            <p class="text-md text-purple-600">${item.amount}</p>
           </div>
       {/each}
     {/if}
