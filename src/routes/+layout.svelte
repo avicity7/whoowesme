51ceb7d94508f5232f94638e194d9b0bd5a11de9
avicity7 <karlorjalo@gmail.com>
2023-10-18 15:47:16 +0800

    chore: Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+  import "../app.css"
+</script>
+
+<nav class="m-8 text-xl">
+	<a href="/" class="mr-6 font-medium text-blue-200 hover:text-blue-400">home</a>
+	<a href="/about" class="mr-6 font-medium text-blue-200 hover:text-blue-400">about</a>
+</nav>
+
+<slot />
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,5 @@
+<div class="mt-20 m-8">
+  <div class="flex flex-row justify-center">
+    <h1 class="text-3xl font-semibold">Welcome to the about page.</h1>
+  </div>
+</div>
