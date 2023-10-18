487792e9b143153d5a5ef59e89f26d1c6caea9f8
avicity7 <karlorjalo@gmail.com>
2023-10-18 20:40:38 +0800

    fix: Add submit button

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -5,6 +5,9 @@
     let result = await data.json()
     pageData = result
   }
+  const sendData = async() => {
+
+  }
 </script>
 
 <div class="font-albert m-16 flex flex-row justify-center text-neutral-800">
@@ -12,6 +15,7 @@
     <h1 class="w-screen pl-24 font-bold text-3xl mb-24">Home</h1>
     <form
       class="flex flex-col items-center"
+      on:submit|preventDefault={sendData}
     >
       <input 
         type="text"
@@ -31,6 +35,12 @@
           class="ml-2 border-2 border-neutral-200 rounded focus:border-purple-400 outline-none select-none"
         />
       </label>
+      <button 
+        class="bg-neutral-800 text-neutral-100 font-semibold rounded hover:bg-purple-900 mt-8 p-2"
+        type="submit"
+      >
+        Sub
+      </button>
     </form>
 
     <button
