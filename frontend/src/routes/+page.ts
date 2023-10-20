272a61c4175c23ebf1156e2bcb196c85965f9d66
avicity7 <karlorjalo@gmail.com>
2023-10-20 22:43:34 +0800

    feat: Display with email

--- frontend/src/routes/+page.ts
+++ frontend/src/routes/+page.ts
@@ -1,8 +1,9 @@
 import { PUBLIC_RENDER_URL } from '$env/static/public'
 
-export async function load() {
+
+export async function load(page) {
   const url = PUBLIC_RENDER_URL as RequestInfo
-  const data = await fetch(url)
+  const data = await fetch(url+`?lender_email=${page.data.session?.user?.email}`)
   let result = await data.json()
   const sum = result.sum
   result = result.results
